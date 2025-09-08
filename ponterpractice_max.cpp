d355bece4d96848a968cf2d4c7bf1c698cca5fff
coaster123 <konstantinoscarrera@gmail.com>
2025-09-08 13:11:04 -0500

    Create ponterpractice_max.cpp

---
+++ ponterpractice_max.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <array>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    
+    srand(time(0));
+
+    const int size = 10;
+    int arr[size];
+
+    for (int i = 0; i < 10; i++) {
+        
+        arr[i] = rand() % 10 + 1;
+        cout << arr[i] << " ";
+
+    }
+    
+    int* max_ptr = &arr[0];
+
+    for (int i = 0; i < 10; i++) {
+        if (arr[i] > *max_ptr) {
+            max_ptr = &arr[i];
+        }
+    }
+
+    cout << "\n\nMax number in this random array of numbers is -> " << *max_ptr;
+
+}
