cbf7db5f4394e7d6cad7e8b067d2177a221371e2
coaster123 <konstantinoscarrera@gmail.com>
2025-10-06 17:07:59 -0500

    Update until.cpp

--- until.cpp
+++ until.cpp
@@ -1,1 +1,43 @@
 
+
+#include <functional>
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+
+
+int until(int x, function<int(int)> f, function<bool(int)> g) {
+    while (!g(x)) {
+        x = f(x);
+    }
+    return x;
+}
+
+int double_number(int n) {
+    return n * 2;
+}
+
+bool greater_than_100(int n) {
+    return n > 100;
+}
+
+bool greater_than_200(int n) {
+    return n > 200;
+}
+
+bool greater_than_500(int n) {
+    return n > 500;
+}
+
+
+int main() {
+
+    assert(until(1, double_number, greater_than_100) == 128);
+    assert(until(1, double_number, greater_than_200) == 256);
+    assert(until(1, double_number, greater_than_500) == 512);
+
+    cout << "PASS!";
+   
+}
+
