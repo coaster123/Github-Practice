e836c93accdb9231b22ff692148404530603e6cf
coaster123 <konstantinoscarrera@gmail.com>
2025-11-25 12:55:04 -0600

    Implement Queue and Quack data structures
    Added Queue and Quack classes with basic operations.

--- datastructures.cpp
+++ datastructures.cpp
@@ -1,1 +1,66 @@
 
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+using namespace std;
+
+
+//Queu
+class Queue {
+private:
+    vector<int> data;
+public:
+    void enqueue(int v) {
+        data.push_back(v);
+    }
+
+    int dequeue() {
+        if (data.empty()) {
+            throw out_of_range("Queue emty");
+        }
+        int x = data.front();
+        data.erase(data.begin());
+        return x;
+    }
+
+    bool empty() const {
+        return data.empty();
+    }
+};
+
+//Wuack
+class Quack {
+private:
+    vector<int> data;
+public:
+    void push_front(int v) {
+        data.insert(data.begin(), v);
+    }
+
+    void push_back(int v) {
+        data.push_back(v);
+    }
+
+    int pop_front() {
+        if (data.empty()) {
+            throw out_of_range("Quack empty");
+        }
+        int x = data.front();
+        data.erase(data.begin());
+        return x;
+    }
+
+    int pop_back() {
+        if (data.empty()) {
+            throw out_of_range("Quack empty");
+        }
+        int x = data.back();
+        data.pop_back();
+        return x;
+    }
+
+    bool empty() const {
+        return data.empty();
+    }
+};
