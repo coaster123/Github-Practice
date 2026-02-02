da4d61a26ddcfd4f36fa44d706d2da21b5cde5bb
coaster123 <konstantinoscarrera@gmail.com>
2026-02-02 13:13:22 -0600

    Update FactorizerTest.java

--- PrimeLab/app/src/test/java/primelab/FactorizerTest.java
+++ PrimeLab/app/src/test/java/primelab/FactorizerTest.java
@@ -8,23 +8,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactorizerTest {
 
-@Test
-void testOdd() {
-    Factorizer factorizer = new Factorizer();
-    assertEquals(factorizer.primeFactors(15), new ArrayList<>(Arrays.asList(3, 5)));
+    @Test
+    void testOdd() {
+        Factorizer factorizer = new Factorizer();
+        assertEquals(factorizer.primeFactors(15), new ArrayList<>(Arrays.asList(3, 5)));
+    }
+
+    @Test
+    void testEven() {
+        Factorizer factorizer = new Factorizer();
+        assertEquals(factorizer.primeFactors(20), new ArrayList<>(Arrays.asList(2, 2, 5)));
+    }
+
+    @Test
+    void testHundred() {
+        Factorizer factorizer = new Factorizer();
+        assertEquals(factorizer.primeFactors(100), new ArrayList<>(Arrays.asList(2, 2, 5, 5)));
+    }
+
 }
 
-@Test
-void testEven() {
-    Factorizer factorizer = new Factorizer();
-    assertEquals(factorizer.primeFactors(20), new ArrayList<>(Arrays.asList(2, 2, 5)));
-}
-
-@Test
-void testHundred() {
-    Factorizer factorizer = new Factorizer();
-    assertEquals(factorizer.primeFactors(100), new ArrayList<>(Arrays.asList(2, 2, 5, 5)));
-}
-
-}
 
