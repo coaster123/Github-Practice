7cbffd0a567a989b1345a700c9f0a59bfdbaf47f
coaster123 <konstantinoscarrera@gmail.com>
2026-02-08 22:23:52 -0600

    Implement clear method in GroceryCounter
    Added clear method to reset totalValue and overflowAmount.

--- Count em up Lab/app/src/main/java/countemuplab/GroceryCounter.java
+++ Count em up Lab/app/src/main/java/countemuplab/GroceryCounter.java
@@ -2,40 +2,44 @@ package countemuplab;
 
 public class GroceryCounter {
 
+
+    
     private int totalValue;
     private int overflowAmount;
 
-    
+
 
     public GroceryCounter() {
         totalValue = 0;
         overflowAmount = 0;
     }
 
-    
+
+
     public void addTens() {
         totalValue = totalValue + 1000;
         checkOverflow();
     }
 
-    
+
 
     public void addOnes() {
         totalValue = totalValue + 100;
         checkOverflow();
     }
-    
 
     public void addTenths() {
         totalValue = totalValue + 10;
         checkOverflow();
     }
 
-    
+
     public void addHundredths() {
         totalValue = totalValue + 1;
         checkOverflow();
     }
+
+
 
     private void checkOverflow() {
         if (totalValue >= 10000) {
@@ -45,17 +49,20 @@ public class GroceryCounter {
     }
 
 
-    
+    public void clear() {
+        totalValue = 0;
+        overflowAmount = 0;
+    }
+
     public double getPrice() {
         return totalValue / 100.0;
     }
 
-    
     public int getOverflows() {
         return overflowAmount;
     }
 
 
     
-
 }
+
