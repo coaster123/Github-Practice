30885924086eaecb8934d181184153f64cce5354
coaster123 <konstantinoscarrera@gmail.com>
2026-02-08 22:16:52 -0600

    Refactor GroceryCounter class formatting

--- Count em up Lab/app/src/main/java/countemuplab/GroceryCounter.java
+++ Count em up Lab/app/src/main/java/countemuplab/GroceryCounter.java
@@ -5,26 +5,33 @@ public class GroceryCounter {
     private int totalValue;
     private int overflowAmount;
 
+    
+
     public GroceryCounter() {
         totalValue = 0;
         overflowAmount = 0;
     }
 
+    
     public void addTens() {
         totalValue = totalValue + 1000;
         checkOverflow();
     }
 
+    
+
     public void addOnes() {
         totalValue = totalValue + 100;
         checkOverflow();
     }
+    
 
     public void addTenths() {
         totalValue = totalValue + 10;
         checkOverflow();
     }
 
+    
     public void addHundredths() {
         totalValue = totalValue + 1;
         checkOverflow();
@@ -37,14 +44,18 @@ public class GroceryCounter {
         }
     }
 
+
+    
     public double getPrice() {
         return totalValue / 100.0;
     }
 
+    
     public int getOverflows() {
         return overflowAmount;
     }
 
 
     
+
 }
