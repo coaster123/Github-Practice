f7e1c796cc4a581ec93825a64148ab5fd6b33f06
coaster123 <konstantinoscarrera@gmail.com>
2026-03-22 20:49:53 -0700

    Add files via upload

---
+++ Hide and Seek/app/src/main/java/visual/studios/code/Search.java
@@ -0,0 +1,44 @@
+package visual.studios.code;
+
+import java.util.ArrayList;
+
+public class Search {
+
+  ArrayList<Fuzzy> fuzzies;
+
+  public Search(ArrayList<Fuzzy> fuzzies) {
+    this.fuzzies = fuzzies;
+  }
+
+  public int linearSearch() {
+    for (int i = 0; i < fuzzies.size(); i++) {
+      if (fuzzies.get(i).color.equals("gold")) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public int binarySearch() {
+    int low = 0;
+    int high = fuzzies.size() - 1;
+
+    while (low <= high) {
+      int mid = (low + high) / 2;
+
+      String color = fuzzies.get(mid).color;
+
+      if (color.equals("gold")) {
+        return mid;
+      }
+
+      if (color.compareTo("gold") < 0) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+
+    return -1;
+  }
+}
